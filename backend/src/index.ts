c6379f72fa5ce869359e4fb8b1d22143c75019b8
tobiaslrsn <nestorxxv@gmail.com>
2022-08-17 19:03:58 +0200

    Check to see if dotenv and typescript is working.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,5 +8,9 @@ app.get("/", (_: Request, res: Response) => {
   res.status(200).send("hi");
 });
 
+const message: string = "Hi, the server is running on ";
+
 let PORT: string | number = process.env.PORT_NR || 8000;
-app.listen(PORT, () => console.log(`http://localhost:${PORT}/`));
+app.listen(PORT, () =>
+  console.log(message + "\x1b[33m%s\x1b[0m", `http://localhost:${PORT}/`)
+);
