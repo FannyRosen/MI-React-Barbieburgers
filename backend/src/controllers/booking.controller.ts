dfcd06417076412bbb0241f6dd3c5929363f3e70
Fanny Rosen <fanny.rosen@hotmail.com>
2022-08-19 10:57:49 +0200

    added post custoner new

--- backend/src/controllers/booking.controller.ts
+++ backend/src/controllers/booking.controller.ts
@@ -28,18 +28,18 @@ export const post_newBookingsController = async (
   res: Response
 ) => {
   try {
-    let { name, email } = req.body;
+    /*     let { name, email } = req.body;
     const postCustomer = new CustomerModel({
       name: name,
       email: email,
     });
     const saveCustomerToDB = await postCustomer.save();
-
+ */
     let { date, sittingTime, clientId } = req.body;
     const postNewBooking = new BookingModel({
       date: date,
       sittingTime: sittingTime,
-      clientId: saveCustomerToDB._id,
+      // clientId: saveCustomerToDB._id,
     });
 
     const saveBookingToDB = await postNewBooking.save();
--- backend/src/controllers/customer.controller.ts
+++ backend/src/controllers/customer.controller.ts
@@ -13,3 +13,30 @@ export const get_customerController = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const post_newCustomerController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    let { name, email, phone } = req.body;
+    const postNewCustomer = new CustomerModel({
+      name: name,
+      email: email,
+      phone: phone,
+    });
+
+    const saveCustomerToDB = await postNewCustomer.save();
+
+    res.status(200).json({
+      status: "statusSuccess",
+      message: "working",
+      data: saveCustomerToDB,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      status: "Failed",
+      message: error,
+    });
+  }
+};
