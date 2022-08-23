f9a3a159d3f7622a43695077d24c6bb9b8b137e0
Fanny Rosen <fanny.rosen@hotmail.com>
2022-08-23 15:21:49 +0200

    added grain styling

--- frontend/src/components/partials/Footer.tsx
+++ frontend/src/components/partials/Footer.tsx
@@ -1,16 +1,24 @@
 import { GlobalStyle } from "../StyledComponents/Fonts";
+import { LogoImage } from "../StyledComponents/Images";
 import { StyledP } from "../StyledComponents/TextElements";
-import { FlexDiv } from "../StyledComponents/Wrappers";
+import { FlexDiv, ImageDiv } from "../StyledComponents/Wrappers";
+import Logo from "../../assets/bb-logo.png";
+import GrainFooter from "../../assets/grain-footer.png";
 
 export const Footer = () => {
   return (
     <>
       <GlobalStyle />
       <FlexDiv background="#006D5D" height="100px">
-        <StyledP textAlign="center" fontsize="20px" color="white">
-          Barbie Burgers <br />
-          AB Copyright 2022
-        </StyledP>
+        <ImageDiv image={GrainFooter}>
+          <FlexDiv dir="column">
+            <LogoImage src={Logo} alt="Logo" width={"150px"} height={"x"} />
+            <StyledP textAlign="center" fontsize="16px" color="white">
+              Barbie Burgers <br />
+              AB Copyright 2022
+            </StyledP>
+          </FlexDiv>
+        </ImageDiv>
       </FlexDiv>
     </>
   );
--- frontend/src/components/partials/Header.tsx
+++ frontend/src/components/partials/Header.tsx
@@ -1,19 +1,21 @@
 import { Navbar } from "../../StyledComponents/Nav/Navbar";
-import { FlexDiv } from "../StyledComponents/Wrappers";
+import { FlexDiv, ImageDiv } from "../StyledComponents/Wrappers";
 import Logo from "../../assets/bb-logo.png";
-
 import { LogoImage } from "../StyledComponents/Images";
+import GrainHeader from "../../assets/grain-header.png";
 
 export const Header = () => {
   return (
     <FlexDiv background="#B992E8" height="160px" dir="column">
-      <LogoImage
-        src={Logo}
-        alt="Logo"
-        width={"250px"}
-        height={"x"}
-        tabletwidth="390px"
-      />
+      <ImageDiv image={GrainHeader}>
+        <LogoImage
+          src={Logo}
+          alt="Logo"
+          width={"250px"}
+          height={"x"}
+          tabletwidth="390px"
+        />
+      </ImageDiv>
       <Navbar></Navbar>
     </FlexDiv>
   );
