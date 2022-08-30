26e419ae45c323c18b1c16cbf6b3c7bedd14843e
Fanny Rosen <fanny.rosen@hotmail.com>
2022-08-30 10:49:52 +0200

    edit functions

--- frontend/src/components/SingleBooking.tsx
+++ frontend/src/components/SingleBooking.tsx
@@ -8,6 +8,9 @@ import {
   fetchBookingByID,
 } from "../services/handleBookingsFetch.service";
 import { fetchCustomerByID } from "../services/handleCustomersFetch.service";
+import { Form } from "./StyledComponents/Form";
+import { StyledLabel } from "./StyledComponents/TextElements";
+import { FlexDiv } from "./StyledComponents/Wrappers";
 
 export const SingleBooking = () => {
   const [bookingById, setBookingById] =
@@ -16,6 +19,10 @@ export const SingleBooking = () => {
     customersDefaultValue
   );
   const [confirmDelete, setConfirmDelete] = useState(false);
+  const [inEdit, setInEdit] = useState(false);
+  const [editNOP, setEditNOP] = useState<number>(0);
+  const [editDate, setEditDate] = useState<Date>(new Date("1999-01-01"));
+  const [editTime, setEditTime] = useState<number>(0);
 
   let params = useParams();
 
@@ -23,7 +30,6 @@ export const SingleBooking = () => {
     fetchCustomerByID(customerById._id)
       .then(async (customerByIdResponse) => {
         setCustomerById(customerByIdResponse.data);
-        console.log(customerByIdResponse);
 
         if (
           /* customerByIdResponse.data._id === bookingById.clientId.toString() */
@@ -41,7 +47,29 @@ export const SingleBooking = () => {
       .catch((error) => {
         console.log(error);
       });
-  }, [bookingById.clientId, customerById._id, params]); // håll kanske tom []
+  }, [params]); // håll kanske tom []
+
+  useEffect(() => {
+    setEditDate(new Date(bookingById.date));
+    setEditNOP(bookingById.numberOfPeople);
+    //setEditTime(bookingById.sittingTime)
+  }, [bookingById]);
+
+  const curr = new Date();
+  curr.setDate(curr.getDate());
+  const inputDate = curr.toISOString().substring(0, 10);
+
+  const handleEditChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditDate(new Date(e.target.value));
+  };
+
+  /*   const handleEditNOP = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditNOP(parseInt(e.currentTarget.value));
+  }; */
+
+  const handleEditNOP = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditNOP(parseInt(e.target.value));
+  };
 
   return (
     <>
@@ -51,10 +79,47 @@ export const SingleBooking = () => {
       <Link to={"/admin/customers/" + customerById._id}>
         <button>GO TO CUSTOMER</button>
       </Link>
-      <p>DATE OF SITTING {bookingById.date.toLocaleString()}</p>
-      <p>WHICH SITTING {bookingById.sittingTime}</p>
-      <p>PEOPLE ON RESERVATION {bookingById.numberOfPeople}</p>
-      <button>Edit</button>
+      {inEdit ? (
+        <>
+          <Form>
+            <FlexDiv dir="column" gap="10px">
+              <StyledLabel>Edit date</StyledLabel>
+              <input
+                type="date"
+                min={inputDate}
+                max={"2023-12-31"}
+                defaultValue={editDate.toLocaleDateString()}
+                onChange={handleEditChange}
+              />
+              <StyledLabel>Edit sitting time</StyledLabel>
+
+              <select name="time" defaultValue={} onChange={}>
+                <option value="1">6.00 pm</option>
+                <option value="2">9.00 pm</option>
+              </select>
+              <StyledLabel>Edit number of people</StyledLabel>
+              <input
+                type="number"
+                defaultValue={editNOP}
+                onChange={handleEditNOP}
+                min="1"
+                max="12"
+              />
+              {/* <button onClick={() => setEditBooking()}>Save</button> */}
+            </FlexDiv>
+          </Form>
+        </>
+      ) : (
+        <>
+          {" "}
+          <p>
+            DATE OF SITTING {new Date(bookingById.date).toLocaleDateString()}
+          </p>
+          <p>WHICH SITTING {bookingById.sittingTime}</p>
+          <p>PEOPLE ON RESERVATION {bookingById.numberOfPeople}</p>
+        </>
+      )}
+      <button onClick={() => setInEdit(true)}>Edit</button>
       {confirmDelete ? (
         <>
           <button onClick={() => deleteBooking(bookingById._id)}>
@@ -75,3 +140,6 @@ export const SingleBooking = () => {
     </>
   );
 };
+function setDate(arg0: Date) {
+  throw new Error("Function not implemented.");
+}
