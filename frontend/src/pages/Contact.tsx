3816b2204fad0d23db86c0e30369ba6179ea1fb3
Fanny Rosen <fanny.rosen@hotmail.com>
2022-08-31 10:16:54 +0200

    added contact styling

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,3 +1,21 @@
+import { Background } from "../components/StyledComponents/Background";
+import { colors } from "../components/StyledComponents/mixins";
+import { FlexDiv } from "../components/StyledComponents/Wrappers";
+
 export const Contact = () => {
-  return <></>;
+  return (
+    <>
+      <Background>
+        <FlexDiv
+          borderRadius="10px"
+          background={colors.LightPink}
+          width="80%"
+          height="min-content"
+          dir="column"
+        >
+          <FlexDiv width="90%" dir="column" margin="20px"></FlexDiv>
+        </FlexDiv>
+      </Background>
+    </>
+  );
 };
