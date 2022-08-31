ba07e8f06c51c690e057bcbd068c28da5fe6322f
tobiaslrsn <nestorxxv@gmail.com>
2022-08-31 14:36:03 +0200

    handling bookings with more than 6 people working.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,29 +11,32 @@ import { SingleBooking } from "./components/SingleBooking";
 import { Customers } from "./components/Customers";
 import { Customer } from "./components/Customer";
 import { AdminBookings } from "./components/AdminBooking";
+import ScrollToTop from "./components/partials/ScrollToTop";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />}></Route>
-            <Route path='/menu' element={<FoodMenu />}></Route>
-            <Route path='/contact' element={<Contact />}></Route>
-            <Route path='/book' element={<Book />}></Route>
-            <Route path='/thankyou' element={<ThankYou />}></Route>
-            <Route path='/admin'>
-              <Route index element={<AdminBookings />}></Route>
-              <Route path=':id' element={<SingleBooking />}></Route>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />}></Route>
+              <Route path="/menu" element={<FoodMenu />}></Route>
+              <Route path="/contact" element={<Contact />}></Route>
+              <Route path="/book" element={<Book />}></Route>
+              <Route path="/thankyou" element={<ThankYou />}></Route>
+              <Route path="/admin">
+                <Route index element={<AdminBookings />}></Route>
+                <Route path=":id" element={<SingleBooking />}></Route>
+              </Route>
+              <Route path="/admin/customers">
+                <Route index element={<Customers />}></Route>
+                <Route path=":id" element={<Customer />}></Route>
+              </Route>
+              <Route path="*" element={<NotFound />}></Route>
             </Route>
-            <Route path='/admin/customers'>
-              <Route index element={<Customers />}></Route>
-              <Route path=':id' element={<Customer />}></Route>
-            </Route>
-            <Route path='*' element={<NotFound />}></Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     </>
   );
