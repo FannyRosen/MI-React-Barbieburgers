70090758ea1839b6dd36dfabd376fc96667edf2a
julialotta <julialottatinglof@gmail.com>
2022-08-31 10:17:26 +0200

    cleaned layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,20 +18,20 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
-            <Route index element={<Home />}></Route>
-            <Route path='/menu' element={<FoodMenu />}></Route>
-            <Route path='/contact' element={<Contact />}></Route>
-            <Route path='/book' element={<Book />}></Route>
-            <Route path='/thankyou' element={<ThankYou />}></Route>
+            <Route index element={<Home />} />
+            <Route path='/menu' element={<FoodMenu />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/book' element={<Book />} />
+            <Route path='/thankyou' element={<ThankYou />} />
             <Route path='/admin'>
-              <Route index element={<AdminBookings />}></Route>
-              <Route path=':id' element={<SingleBooking />}></Route>
+              <Route index element={<AdminBookings />} />
+              <Route path=':id' element={<SingleBooking />} />
             </Route>
             <Route path='/admin/customers'>
-              <Route index element={<Customers />}></Route>
-              <Route path=':id' element={<Customer />}></Route>
+              <Route index element={<Customers />} />
+              <Route path=':id' element={<Customer />} />
             </Route>
-            <Route path='*' element={<NotFound />}></Route>
+            <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
