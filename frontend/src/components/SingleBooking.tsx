4f2215441b6789a091dde0cb20ad5fd6c5bb4d44
julialotta <julialottatinglof@gmail.com>
2022-09-07 09:38:39 +0200

    cleaned in singleBooking

--- frontend/src/components/SingleBooking.tsx
+++ frontend/src/components/SingleBooking.tsx
@@ -1,23 +1,16 @@
-import axios from "axios";
-import React, { ChangeEvent, useEffect, useState } from "react";
-import { Link, matchRoutes, useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import { bookingsDefaultValue, IBooking } from "../models/IBooking";
 import { customersDefaultValue, ICustomer } from "../models/ICustomer";
-import {
-  deleteBooking,
-  editBooking,
-  fetchBookingByID,
-} from "../services/handleBookingsFetch.service";
+import { fetchBookingByID } from "../services/handleBookingsFetch.service";
 import { fetchCustomerByID } from "../services/handleCustomersFetch.service";
-import { checkAvailableSittings, ISittings } from "../services/utils";
+import { ISittings } from "../services/utils";
 import { UpdateBooking } from "./admin/UpdateBooking";
 import { BookingDeleteButton } from "./BookingDeleteButton";
 import { SingleBookingRender } from "./SingleBookingRender";
 import { Background } from "./StyledComponents/Background";
-import { Form } from "./StyledComponents/Form";
 import { colors } from "./StyledComponents/mixins";
 import { StyledButton } from "./StyledComponents/StyledButton";
-import { StyledLabel, StyledP } from "./StyledComponents/TextElements";
 import { FlexDiv } from "./StyledComponents/Wrappers";
 
 export const SingleBooking = () => {
@@ -29,12 +22,6 @@ export const SingleBooking = () => {
   );
   const [inEdit, setInEdit] = useState(false);
 
-  const [editNOP, setEditNOP] = useState<number>(0);
-  const [editDate, setEditDate] = useState<Date>(new Date("1999-01-01"));
-  const [editSittingTime, setEditSittingTime] = useState<number>(0);
-  const [isAvailable, setIsAvailable] = useState<ISittings>();
-  const [confirmEdit, setConfirmEdit] = useState<IBooking>(bookingById);
-
   let params = useParams();
   const location = useLocation();
   const adminPath = location.pathname === "/admin/" + bookingById._id;
@@ -45,139 +32,35 @@ export const SingleBooking = () => {
       .then(async (customerByIdResponse) => {
         setCustomerById(customerByIdResponse.data);
 
-        if (
-          /* customerByIdResponse.data._id === bookingById.clientId.toString() */
-          bookingById.clientId!.toString() === customerByIdResponse.data._id
-        ) {
-          console.log("stämmer");
-        } else {
-          console.log("fel");
-        }
-
         const bookingsByIdResponse = await fetchBookingByID(params.id!);
         setBookingById(bookingsByIdResponse.data);
-        console.log(bookingsByIdResponse);
-
-        /*  const postUpdatedBooking = editBooking(params.id!);
-        setConfirmEdit(bookingById);
-        console.log(postUpdatedBooking); */
       })
 
       .catch((error) => {
         console.log(error);
       });
-  }, [bookingById.clientId, customerById._id, params]);
-
-  /* 
-  useEffect(() => {
-    setEditDate(new Date(bookingById.date));
-    setEditNOP(bookingById.numberOfPeople);
-    setEditSittingTime(bookingById.sittingTime);
-  }, [bookingById]);
-
-  useEffect(() => {
-    const checkDate = async () => {
-      const isAvailableinDB = await checkAvailableSittings(editDate);
-      setIsAvailable(isAvailableinDB);
-    };
-    checkDate().catch(console.error);
-    console.log(isAvailable);
-  }, [editDate]);
-
-  const curr = new Date();
-  curr.setDate(curr.getDate());
-  const inputDate = curr.toISOString().substring(0, 10);
-  
-  const updateBooking = (e: ChangeEvent<HTMLFormElement>) => {
-    editBooking(e.target.value)
-  };
-
-  
-  const handleEditChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditDate(new Date(e.target.value));
-    const isAvailableinDB = await checkAvailableSittings(editDate);
-    setIsAvailable(isAvailableinDB);
-  };
-
-  const handleEditNOP = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEditNOP(parseInt(e.target.value));
-  };
-
-  const handleEditSittingTime = async (
-    e: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setEditSittingTime(parseInt(e.target.value));
-  };
-
- */
+  }, []);
 
   return (
     <>
       <Background>
         <FlexDiv
-          borderRadius="10px"
+          borderRadius='10px'
           background={colors.LightPink}
-          width="80%"
-          height="min-content"
-          dir="column"
-          padding="40px"
+          width='80%'
+          height='min-content'
+          dir='column'
+          padding='40px'
         >
           {adminPath ? (
             <>
               {inEdit ? (
                 <>
-                  {/* <Form>
-                    <FlexDiv dir="column" gap="10px">
-                      <StyledLabel>Edit date</StyledLabel>
-                      <input
-                        type="date"
-                        min={inputDate}
-                        max={"2023-12-31"}
-                        defaultValue={editDate.toLocaleDateString()}
-                        onChange={handleEditChange}
-                      />
-                      <StyledLabel>Edit sitting time</StyledLabel>
-
-                      <select
-                        name="time"
-                        defaultValue={editSittingTime.toString()}
-                        onChange={handleEditSittingTime}
-                      >
-                        {isAvailable?.firstSitting ? (
-                          <option value="1">6.00 pm</option>
-                        ) : (
-                          <option>not available</option>
-                        )}
-                        {isAvailable?.secondSitting ? (
-                          <option value="1">9.00 pm</option>
-                        ) : (
-                          <option>not available</option>
-                        )}
-                      </select>
-
-                      <StyledLabel>Edit number of people</StyledLabel>
-                      <input
-                        type="number"
-                        defaultValue={editNOP}
-                        onChange={handleEditNOP}
-                        min="1"
-                        max="12"
-                      />
-                      <button onSubmit={() => editBooking(bookingById._id)}>
-                        Save
-                      </button>
-                    </FlexDiv>
-                  </Form>  */}
-
-                  {/* UPDATE FORM */}
-
                   <UpdateBooking></UpdateBooking>
 
-                  {/* !UPDATE FORM */}
-
                   <StyledButton
-                    width="70px"
-                    height="30px"
+                    width='70px'
+                    height='30px'
                     onClick={() => setInEdit(false)}
                   >
                     Back
@@ -185,10 +68,10 @@ export const SingleBooking = () => {
                 </>
               ) : (
                 <>
-                  <FlexDiv gap="10px">
+                  <FlexDiv gap='10px'>
                     <StyledButton
-                      width="70px"
-                      height="30px"
+                      width='70px'
+                      height='30px'
                       onClick={() => setInEdit(true)}
                     >
                       Edit
--- frontend/src/components/admin/UpdateBooking.tsx
+++ frontend/src/components/admin/UpdateBooking.tsx
@@ -34,7 +34,6 @@ export const UpdateBooking = () => {
   }, []);
 
   const submitUpdatedBooking = (e: FormEvent) => {
-    console.log("submit");
     e.preventDefault();
     let updateBooking = {
       date: new Date(date),
@@ -63,43 +62,43 @@ export const UpdateBooking = () => {
         <></>
       ) : (
         <Form onSubmit={submitUpdatedBooking}>
-          <FlexDiv dir="column" gap="10px">
+          <FlexDiv dir='column' gap='10px'>
             <StyledLabel>Choose a date</StyledLabel>
             <MyCalendar handleDate={handleDateChange} />
             <Label>Number of people</Label>
             <StyledSelect
               required
-              name="numberOfPeople"
+              name='numberOfPeople'
               onChange={handleNOPChange}
               defaultValue={bookingById.numberOfPeople}
             >
-              <option disabled value="0">
+              <option disabled value='0'>
                 0
               </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              <option value='6'>6</option>
+              <option value='7'>7</option>
+              <option value='8'>8</option>
+              <option value='9'>9</option>
+              <option value='10'>10</option>
+              <option value='11'>11</option>
+              <option value='12'>12</option>
             </StyledSelect>
             <StyledSelect
               required
-              id="date"
-              name="date"
+              id='date'
+              name='date'
               onChange={handleSittingTimeChange}
               defaultValue={bookingById.sittingTime}
             >
-              <option value="1">6.00 pm</option>
-              <option value="2">9.00 pm</option>
+              <option value='1'>6.00 pm</option>
+              <option value='2'>9.00 pm</option>
             </StyledSelect>
-            <Input type="submit" value={"update"} />
+            <Input type='submit' value={"update"} />
           </FlexDiv>
         </Form>
       )}
